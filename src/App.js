f309cb8dbf7a17f99949d49146671fee7e0b0379
korrio <iamnotkorr@gmail.com>
2022-12-01 11:58:09 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useLottie, useLottieInteractivity } from "lottie-react";
+import bgAnimation from './anims/BG.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const options = {
+  animationData: bgAnimation,
+};
+
+const style = {
+  border: 1,
+  borderStyle: "solid",
+  borderRadius: 0,
+  borderColor: "#fff"
+};
+
+const App = () => {
+  const lottieObj = useLottie(options, style);
+  const Animation = useLottieInteractivity({
+    lottieObj,
+    mode: "scroll",
+    actions: [
+      {
+        visibility: [0, 0.45],
+        type: "stop",
+        frames: [0],
+      },
+      {
+        visibility: [0.45, 1],
+        type: "seek",
+        frames: [0, 38],
+      },
+    ],
+  });
+
+  return Animation;
+};
 
 export default App;
